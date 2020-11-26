1835d3904ff8f25d9ad815c1d35eafcd62986d08
Evana Margain Puig <evanamp233@gmail.com>
2020-11-26 16:18:40 -0600

    Sets the dice images to the result in the randomizer

--- app/src/main/java/com/evanamargain/diceroller/MainActivity.kt
+++ app/src/main/java/com/evanamargain/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.evanamargain.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.core.graphics.drawable.toDrawable
 import com.evanamargain.diceroller.databinding.ActivityMainBinding
 import kotlin.random.Random
 
@@ -15,15 +16,23 @@ class MainActivity : AppCompatActivity() {
     val view = binding.root
     setContentView(view)
 
-    binding.rollButton.text = "Let's roll"
     binding.rollButton.setOnClickListener {
       rollDice()
     }
   }
 
   private fun rollDice() {
-    val randomInt = Random.nextInt(6) + 1
 
-    binding.resultText.text = randomInt.toString()
+    val drawableResource = when (Random.nextInt(6) + 1) {
+      1 -> R.drawable.dice_1
+      2 -> R.drawable.dice_2
+      3 -> R.drawable.dice_3
+      4 -> R.drawable.dice_4
+      5 -> R.drawable.dice_5
+      else -> R.drawable.dice_6
+    }
+
+    binding.diceImage.setImageResource(drawableResource)
+
   }
 }
